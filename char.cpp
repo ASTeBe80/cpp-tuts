a0a4ac370b60ec6f69112ffc67f1581bac9950f0
ASTeBe80 <astebe80@gmail.com>
2024-07-15 19:23:11 +0530

    some files

---
+++ char.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //char
+    char ch;    // for single char only
+    cin >> ch;
+    cout<< ch <<endl;
+
+    char ch2 = 'a'; //char is stored in single quotes (' ')
+    cout << ch2 <<endl;
+
+    string ch3 = "a"; // string uses double quotes (" ")
+    cout << ch3<<endl;
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //int
+    int x = 10;
+
+    //long
+    long a = 2000;
+    
+    long long b = 200000000;
+
+    //float, double
+    float y = 5.0; //works with decimal
+    float z = 6;    // works without decimal as well
+    double c =7;    // int can also be stored in double
+
+    cout << x << endl << a << endl << b << endl << y << endl << z << endl << c << endl;
+
+    return 0;
+
+    
+}
